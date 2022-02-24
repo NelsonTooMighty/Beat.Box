135d6d89d411af15e5fd401c1cf3df8da517ef66
Avery VanAusdal <arv107@txstate.edu>
2022-02-24 02:12:45 -0600

    Barebones Song class implementation

---
+++ src/Song.java
@@ -0,0 +1,9 @@
+public class Song { //documentation: https://developer.spotify.com/documentation/web-api/reference/#/operations/get-track, https://developer.apple.com/documentation/musickit/track/
+    String trackName;
+    String artistName;
+    String albumName;
+    String albumArtURL;
+    String releaseDate;
+    boolean isLocal;
+    String localPath;
+}
