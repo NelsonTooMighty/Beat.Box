dc74c96dee0d4d9bc34a99dc250d2625a21a00c3
libin96 <lbk17@txstate.edu>
2022-03-04 14:35:01 -0600

    BB-25: Favorite Playlist Creator start

---
+++ src/main/FavoritePlaylistCreator.java
@@ -0,0 +1,41 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class FavoritePlaylistCreator extends LayoutFrame {
+
+    public void createLikeList(){
+
+    Playlist likedPlaylist = new Playlist();
+    Database currentDatabase = Database.getInstance(); // gets access to user's database
+    ImageIcon dislikeImage = new ImageIcon("GreyBox.png"),  
+               likeImage = new ImageIcon("beatBox.png");
+    // got code above from https://www.oreilly.com/library/view/java-swing/156592455X/ch04s02.html
+    Boolean isLiked = false;
+    JButton likeButton = new JButton(dislikeImage);
+    Song currentSong; //supposed to hold current song being Played
+    currentDatabase.add(likedPlaylist);
+
+    
+    ActionListener likeNow = new ActionListener(){
+        @Override
+        public void actionPerformed(ActionEvent e){
+            if (isLiked){ 
+                likeButton.setIcon(dislikeImage);
+                removeSong();
+            }
+            else{
+                likeButton.setIcon(likeImage);
+                addSong();
+            }
+        }
+    }
+    private void addSong(){likedPlaylist.add(currentSong);}
+    private void removeSong(){likedPlaylist.remove(currentSong);}
+    
+    }
+
+    
+    
+}
