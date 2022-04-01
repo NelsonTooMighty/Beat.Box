b74469b413b7bea25d36f0fcb8d6874fde7ee63d
arv107 <arv107@txstate.edu>
2022-03-31 22:09:07 -0500

    BB-69: Updated CloudPlaylistImporter

--- src/main/CloudPlaylistImporter.java
+++ src/main/CloudPlaylistImporter.java
@@ -1,2 +1,4 @@
-package PACKAGE_NAME;public interface CloudPlaylistImporter {
+public interface CloudPlaylistImporter {
+    Playlist importPlaylist(String url);
 }
+//reference for Spotify: https://github.com/spotify-web-api-java/spotify-web-api-java
