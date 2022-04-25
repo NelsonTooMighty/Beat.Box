a9a5f25b4c8ec8b042e4826bd1545cc5845169ed
Nelson Uzoaru <n_u18@txstate.edu>
2022-04-25 11:29:28 -0500

    SongIdentifierTest

--- src/test/SongIdentifierTest.java
+++ src/test/SongIdentifierTest.java
@@ -4,4 +4,13 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 public class SongIdentifierTest {
+        @Test
+        @DisplayName("Test for Song Class With No Parameters")
+        public void SongIdentifierTest() throws IOException, ClassNotFoundException
+        {
+            final Song TestSong= new Song();
+            TestSong.setSongName("Power");
+            assertTrue(true== TestSong.isLocal());
+
+        }
 }
