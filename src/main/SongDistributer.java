556f2731f9cead9f713f5478753191fdb44de056
Austin Sansing <acs190@txstate.edu>
2022-04-25 16:37:53 -0500

    BB-71
    A song distributor to take a song once it has been identified and add it to a playlist

---
+++ src/main/SongDistributer.java
@@ -0,0 +1,10 @@
+public class SongDistributer {
+    SpotifyAuth spotifyAuth = SpotifyAuth.getInstance();
+
+    public Song distributer(String name) {
+        Playlist pl = newPlaylist(); // pl temp playlist
+        Song s = distributer.identify(name); // s = identify(name) from song identifier
+        pl.remove(s); // remove the song from temp playlist
+        append.s(Playlist) // add song to playlist parent
+            return Playlist; // return playlist
+}
