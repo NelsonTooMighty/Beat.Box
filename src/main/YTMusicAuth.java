bdb98844a025f68ba9e6ba6f18e1282ffda93ee8
Avery VanAusdal <arv107@txstate.edu>
2022-04-25 20:43:37 -0500

    BB-81: Added comments, cleaned up
    Removed singleton pattern, unnecessary

--- src/main/YTMusicAuth.java
+++ src/main/YTMusicAuth.java
@@ -1,38 +1,39 @@
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.services.CommonGoogleClientRequestInitializer;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.YouTubeRequestInitializer;
-import com.google.api.services.youtube.model.PlaylistItemListResponse;
-import com.google.auth.oauth2.GoogleCredentials;
-
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
 
 //Uses Youtube API https://github.com/googleapis/google-api-java-client-services/tree/main/clients/google-api-services-youtube
+
+/**
+ * Holds a YouTube service variable including the API Key for use for imports.
+ */
 public class YTMusicAuth {
     private final String APIKey = "AIzaSyAJ9BHXvMWX0PM6CMs43qKCTmrtiFwzFW0";
-    private static final YTMusicAuth singleton = getInstance();
     private YouTube youtube;
 
-    private YTMusicAuth() {
+    /**
+     * Sets up YouTube service variable for use by other classes.
+     */
+    public YTMusicAuth() {
         try {
             youtube = new YouTube.Builder(
                     GoogleNetHttpTransport.newTrustedTransport(),
                     GsonFactory.getDefaultInstance(),
                     null)
                     .setApplicationName("Beat Box")
-                    .setYouTubeRequestInitializer(new YouTubeRequestInitializer(APIKey))
+                    .setYouTubeRequestInitializer(new YouTubeRequestInitializer(APIKey)) //adds API key for subsequent requests
                     .build();
         } catch (GeneralSecurityException | IOException e) {
             System.out.println("Error setting up YouTube request!:\n\t" + e.getMessage());
         }
     }
 
-    public static YTMusicAuth getInstance() {return singleton;}
+    /**
+     * Gets the Youtube Service with preloaded API key.
+     */
     public YouTube getYoutubeService() {
         return youtube;
     }
